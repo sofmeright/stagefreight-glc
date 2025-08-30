9af6bc5d8945862501cd33904b5e774e8897ce72
SoFMeRight <kai.hamil@gmail.com>
2025-08-30 16:42:41 +0000

    Remove hardcoded values in ..release-notes.sh. Release notes generation is much more dynamic now.

--- scripts/gitlab/generate-release_notes.sh
+++ scripts/gitlab/generate-release_notes.sh
@@ -19,25 +19,31 @@ PREV_RELEASE=$(git rev-list --max-parents=0 "${RELEASE}^")
 NOTABLE_CHANGES=$(git tag -l --format='%(contents)' "$RELEASE" | sed '/-----BEGIN PGP SIGNATURE-----/,//d' | tail -n +6)
 CHANGELOG=$(git log --no-merges --pretty=format:'- [%h] %s (%aN)' "${PREV_RELEASE}..${RELEASE}")
 
+# Fallbacks for local runs
+PROJECT_NAME="${CI_PROJECT_NAME:-$(basename "$(git rev-parse --show-toplevel 2>/dev/null || echo .)")}"
+PROJECT_URL="${CI_PROJECT_URL:-$(git config --get remote.origin.url 2>/dev/null | sed 's/\.git$//')}"
+
 cat <<EOF
-# AntParade GitOps 🐜 - ${CI_PROJECT_NAME}:${CI_COMMIT_TAG}
+# AntParade GitOps 🐜 - ${PROJECT_NAME}:${RELEASE}
 
 ${NOTABLE_CHANGES}
 
-# Image Availability:
+# Image Availability
 
-- [Docker Hub (docker.io)](https://hub.docker.com/r/prplanit/ansible-oci/tags)
+If container images were built and pushed for this release, the registry links are listed in the **Assets** section of this Release (above).  
+Links are added automatically for any configured registries (Docker Hub, Quay, JFrog, GHCR, GitLab Container Registry, or generic OCI).
 
 ## Installation
 
-For installation and usage instructions, please refer to the [README](https://gitlab.prplanit.com/precisionplanit/ansible-oci/-/blob/${RELEASE}/README.md)
+For installation and usage instructions, please refer to the [README](${PROJECT_URL}/-/blob/${RELEASE}/README.md)
 
 ## Contributing
 
 If you find this image useful, you can help:
 
 - Submit a Merge Request with new features or fixes
-- Report bugs or issues on [GitLab Issues](https://gitlab.prplanit.com/precisionplanit/ansible-oci/-/issues)
+- Report bugs or issues on [Issues](${PROJECT_URL}/-/issues)
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/T6T41IT163)
 
 ## Changelog
 
